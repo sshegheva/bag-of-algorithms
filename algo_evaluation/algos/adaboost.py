8fba73488cee8af5e8d3027e2fe8da2f6ca14864
Snejana Shegheva <sshegheva@dataxu.com>
2015-01-14 10:37:09 -0500

    Add AdaBoost Algorithm

--- algo_evaluation/algos/adaboost.py
+++ algo_evaluation/algos/adaboost.py
@@ -1,33 +1,34 @@
 
 """
-Evaluate K nearest neighbours
+Evaluate Ada Boost classifier
 """
 import numpy as np
 import pandas as pd
 
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from algo_evaluation.datasets import split_dataset, load_higgs_train
 from algo_evaluation import LOGGER
 
 
-class KNN:
-    def __init__(self, data, n_neighbours=3):
+class AdaBoost:
+    def __init__(self, data, n_estimators=50, learning_rate=1.0):
         features, weights, labels = data
-        self.clf = KNeighborsClassifier(n_neighbors=n_neighbours)
-        LOGGER.info('Created KNN classifier with %s neighbours', n_neighbours)
+        self.clf = AdaBoostClassifier(n_estimators=n_estimators, learning_rate=learning_rate)
+        LOGGER.info('Created Ada Boost classifier with params {}'.format({'n_estimators': n_estimators,
+                                                                          'learning_rate': learning_rate}))
         self.predictions, self.trnaccuracy, self.tstaccuracy = None, None, None
         self.dataset = split_dataset(features, weights, labels)
 
     def train(self):
         """
-        Train K nearest neighbours on the higgs dataset
+        Train Ada Boost on the higgs dataset
         """
         self.clf = self.clf.fit(self.dataset['training']['features'], self.dataset['training']['labels'])
-        LOGGER.info('Trained KNN classifier')
+        LOGGER.info('Trained Ada Boost classifier')
 
     def predict(self):
         """
-        Predict label using KNN
+        Predict label using Ada Boost
         :return:
         """
         self.predictions = self.clf.predict(self.dataset['test']['features'])
@@ -44,27 +45,27 @@ class KNN:
         LOGGER.info('Test Weighted Accuracy score = %s', self.tstaccuracy)
 
 
-def run_knn(data, n_neighbours):
+def run_AdaBoost(data, n_estimators=50, learning_rate=1.0):
     """
-    Run and evaluate KNN with default settings
+    Run and evaluate Ada Boost with default settings
     """
-    dt = KNN(data=data, n_neighbours=n_neighbours)
+    dt = AdaBoost(data=data, n_estimators=n_estimators, learning_rate=learning_rate)
     dt.train()
     dt.predict()
     dt.evaluate()
     return dt.trnaccuracy, dt.tstaccuracy
 
 
-def estimate_best_n_neighbours():
+def estimate_best_n_estimators():
     """
-    Run KNN classifier with multiple settings of
-    n_neighbours and plot the accuracy function of n_neighbours
-    :return: the best n_neighbours setting
+    Run Ada Boost classifier with multiple settings of
+    n_estimators and plot the accuracy function of n_estimators
+    :return: the best n_estimators setting
     """
-    n_neighbours_range = np.arange(1, 20, 2)
+    n_estimators_range = np.arange(1, 50, 2)
     data = load_higgs_train()
-    records = [[n_neighbours] + list(run_knn(data=data, n_neighbours=n_neighbours))
-               for n_neighbours in n_neighbours_range]
+    records = [[n_estimator] + list(run_AdaBoost(data=data, n_estimators=n_estimator))
+               for n_estimator in n_estimators_range]
     LOGGER.info('Performed evaluation of the n_neighbours setting choice')
     columns = ['n_neighbours', 'training_score', 'test_score']
     df = pd.DataFrame.from_records(records, columns=columns, index=columns[0])
@@ -75,6 +76,3 @@ def estimate_best_n_neighbours():
 def plot_accuracy_function(df):
     smooth_df = pd.rolling_mean(df, 5)
     smooth_df.plot(title='Accuracy change as a function of n_neighbours (smoothed)')
-
-
-

8ba159ca69ea03c22fd606b0cc47918137b7757a
Snejana Shegheva <sshegheva@dataxu.com>
2015-01-17 18:09:08 -0500

    Add entropy to the decision tree

--- algo_evaluation/algos/decision_tree.py
+++ algo_evaluation/algos/decision_tree.py
@@ -1,14 +1,13 @@
-import time
 import pandas as pd
 from sklearn import tree
+import matplotlib.pyplot as plt
 from algo_evaluation.datasets import split_dataset, load_higgs_train
-from algo_evaluation import LOGGER
 
 
 class DecisionTree:
-    def __init__(self, data, min_samples_split=60):
+    def __init__(self, data, criterion='gini', min_samples_split=60):
         features, weights, labels = data
-        self.clf = tree.DecisionTreeClassifier(min_samples_split=min_samples_split)
+        self.clf = tree.DecisionTreeClassifier(criterion=criterion, min_samples_split=min_samples_split)
         self.predictions, self.trnaccuracy, self.tstaccuracy = None, None, None
         self.dataset = split_dataset(features, weights, labels)
 
@@ -34,11 +33,11 @@ class DecisionTree:
                                           sample_weight=self.dataset['test']['weights'])
 
 
-def run_decision_tree(data, min_samples_split=60):
+def run_decision_tree(data, criterion='gini', min_samples_split=60):
     """
     Run and evaluate decision trees with default settings
     """
-    dt = DecisionTree(data=data, min_samples_split=min_samples_split)
+    dt = DecisionTree(data=data, criterion=criterion, min_samples_split=min_samples_split)
     dt.train()
     dt.predict()
     dt.evaluate()
@@ -53,16 +52,21 @@ def estimate_best_min_samples_split():
     """
     min_split_range = xrange(2, 120, 2)
     data = load_higgs_train()
-    records = [[min_sample] + list(run_decision_tree(data=data, min_samples_split=min_sample))
+    records = [[min_sample] + list(run_decision_tree(data=data, criterion='gini', min_samples_split=min_sample))
+               + list(run_decision_tree(data=data, criterion='entropy', min_samples_split=min_sample))
                for min_sample in min_split_range]
-    columns = ['min_sample_split', 'training_score', 'test_score']
+    columns = ['min_sample_split', 'gini_training_score', 'gini_test_score',
+               'entropy_training_score', 'entropy_test_score']
     df = pd.DataFrame.from_records(records, columns=columns, index=columns[0])
     return df
 
 
-def plot_accuracy_function(df):
-    smooth_df = pd.rolling_mean(df, 5)
-    smooth_df.plot(title='Accuracy change as a function of min_samples_split (smoothed)')
+def plot_accuracy_function(df, smoothing_factor=5):
+    gini_df = df[['gini_training_score', 'gini_test_score']]
+    entropy_df = df[['entropy_training_score', 'entropy_test_score']]
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16, 8), sharex=False, sharey=True)
+    pd.rolling_mean(gini_df, smoothing_factor).plot(ax=axes[0], title='Accuracy change as a function of min_samples_split (smoothed)')
+    pd.rolling_mean(entropy_df, smoothing_factor).plot(ax=axes[1], title='Accuracy change as a function of min_samples_split (smoothed)')
 
 
 

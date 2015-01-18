f38bb5fe710ebe9c5978c29e72d15656a2be6c5d
Snejana Shegheva <sshegheva@dataxu.com>
2015-01-17 19:57:46 -0500

    Refactor neural network

--- algo_evaluation/algos/neural_network.py
+++ algo_evaluation/algos/neural_network.py
@@ -3,9 +3,10 @@ import pandas as pd
 from pybrain.datasets import ClassificationDataSet
 from pybrain.utilities import percentError
 from pybrain.tools.validation import Validator
-from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure.modules import SoftmaxLayer
+from pybrain.structure.connections import FullConnection
+from pybrain.structure.modules import SigmoidLayer
+from pybrain.structure.networks import FeedForwardNetwork
 
 from algo_evaluation.datasets import load_higgs_train
 from algo_evaluation import LOGGER, TEST_DATA_SPLIT
@@ -16,9 +17,6 @@ class NeuralNetwork:
         self.features, self.weights, labels = data
         self.labels = np.array([1 if l == 's' else 0 for l in labels])
         self.sample_size = self.features.shape[0]
-        LOGGER.info('Loaded features %s', self.features.shape)
-        LOGGER.info('Loaded weights %s', self.weights.shape)
-        LOGGER.info('Loaded labels %s', self.labels.shape)
         self._prepare_data()
         self._create_trainer()
 
@@ -28,21 +26,40 @@ class NeuralNetwork:
         for i in range(self.sample_size):
             ds.addSample(self.features.iloc[i], self.labels[i])
         self.trndata, self.tstdata = ds.splitWithProportion(TEST_DATA_SPLIT)
-        LOGGER.info("Number of training patterns: %s", len(self.trndata))
-        LOGGER.info("Input and output dimensions: %s, %s", self.trndata.indim, self.trndata.outdim)
 
     def _create_trainer(self):
-        self.fnn = buildNetwork(self.trndata.indim, 2, self.trndata.outdim, outclass=SoftmaxLayer)
-        self.trainer = BackpropTrainer(self.fnn, dataset=self.trndata, momentum=0.1, verbose=True, weightdecay=0.01)
+        self.fnn = FeedForwardNetwork()
+        inLayer = SigmoidLayer(self.trndata.indim)
+        hiddenLayer = SigmoidLayer(int(self.trndata.indim))
+        outLayer = SigmoidLayer(self.trndata.outdim)
 
-    def train(self, train_epoch):
+        self.fnn.addInputModule(inLayer)
+        self.fnn.addModule(hiddenLayer)
+        self.fnn.addOutputModule(outLayer)
+
+        in_to_hidden = FullConnection(inLayer, hiddenLayer)
+        hidden_to_out = FullConnection(hiddenLayer, outLayer)
+
+        self.fnn.addConnection(in_to_hidden)
+        self.fnn.addConnection(hidden_to_out)
+        self.fnn.sortModules()
+
+        self.trainer = BackpropTrainer(self.fnn, dataset=self.trndata,
+                                       momentum=0.1,
+                                       verbose=False,
+                                       weightdecay=0.01,
+                                       learningrate=1.0)
+
+    def train(self, train_epoch=5):
         self.trainer.trainEpochs(train_epoch)
 
-    def predict(self, dataset):
+    def predict(self, dataset=None):
+        if dataset is None:
+            dataset = self.tstdata
         return self.fnn.activateOnDataset(dataset)
 
     def estimate_error(self):
-        trnerror = percentError(self.trainer.testOnClassData(), self.trndata['class'])
+        trnerror = percentError(self.trainer.testOnClassData(dataset=self.trndata), self.trndata['class'])
         tsterror = percentError(self.trainer.testOnClassData(dataset=self.tstdata), self.tstdata['class'])
         LOGGER.info("epoch: %4d", self.trainer.totalepochs)
         LOGGER.info("train error: %5.4f", trnerror)
@@ -59,22 +76,26 @@ class NeuralNetwork:
         return self.trainer.totalepochs, trnaccuracy, tstaccuracy
 
 
+def run_neural_net(data):
+    """
+    Run and evaluate neural networks
+    """
+    nn = NeuralNetwork(data)
+    nn.train()
+    nn.predict()
+    nn.estimate_error()
 
 
 def estimate_training_iterations():
     data = load_higgs_train()
     nn = NeuralNetwork(data)
     error_data = []
-    accuracy_data = []
-    for i in range(20):
+    for i in range(5):
         nn.train(train_epoch=1)
         total_epochs, trnerror, tsterror = nn.estimate_error()
-        total_epochs, trnaccuracy, tstaccuracy = nn.estimate_accuracy()
         error_data.append([total_epochs, trnerror, tsterror])
-        accuracy_data.append([total_epochs, trnaccuracy, tstaccuracy])
     err_df = pd.DataFrame.from_records(error_data, columns=['iteration', 'training_error', 'test_error'])
-    acc_df = pd.DataFrame.from_records(error_data, columns=['iteration', 'training_accuracy', 'test_accuracy'])
-    return err_df, acc_df
+    return err_df
 
 
 def plot_accuracy_function(df, title):

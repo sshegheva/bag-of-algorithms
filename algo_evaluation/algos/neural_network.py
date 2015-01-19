d86b55294637ca601323946ad08258b51aa2a83a
Snejana Shegheva <sshegheva@dataxu.com>
2015-01-19 14:31:56 -0500

    Refactoring neural nets

--- algo_evaluation/algos/neural_network.py
+++ algo_evaluation/algos/neural_network.py
@@ -11,12 +11,14 @@ from algo_evaluation import TEST_DATA_SPLIT
 
 
 class NeuralNetwork:
-    def __init__(self, data,):
+    def __init__(self, data, learning_rate=0.1, momentum=0.1):
         self.features, self.weights, labels = data
         self.labels = np.array([1 if l == 's' else 0 for l in labels])
         self.sample_size = self.features.shape[0]
         self._prepare_data()
-        self._create_trainer()
+        self._create_trainer(learning_rate, momentum)
+        self.learning_rate = learning_rate
+        self.momentum = momentum
 
     def _prepare_data(self):
         classes = set(self.labels)
@@ -27,7 +29,7 @@ class NeuralNetwork:
         self.tstdata._convertToOneOfMany()
         self.trndata._convertToOneOfMany()
 
-    def _create_trainer(self):
+    def _create_trainer(self, learning_rate, momentum):
         self.fnn = FeedForwardNetwork()
         inLayer = SigmoidLayer(self.trndata.indim)
         hiddenLayer = SigmoidLayer(int(self.trndata.indim * 0.5))
@@ -45,10 +47,10 @@ class NeuralNetwork:
         self.fnn.sortModules()
 
         self.trainer = BackpropTrainer(self.fnn, dataset=self.trndata,
-                                       momentum=0.99,
+                                       momentum=momentum,
                                        verbose=False,
                                        weightdecay=0.01,
-                                       learningrate=0.001)
+                                       learningrate=learning_rate)
 
     def train(self, train_epoch=5):
         self.trainer.trainEpochs(train_epoch)
@@ -64,29 +66,35 @@ class NeuralNetwork:
         return self.trainer.totalepochs, trnerror, tsterror
 
 
-def run_neural_net(data):
+def run_neural_net(data, learning_rate=0.1):
     """
     Run and evaluate neural networks
     """
-    nn = NeuralNetwork(data)
+    nn = NeuralNetwork(data, learning_rate=learning_rate)
     nn.train()
     nn.predict()
     return nn.estimate_error()
 
 
-def estimate_training_iterations():
+def estimate_training_iterations(n_iterations=10, learning_rate_range=tuple([0.01, 0.1, 1.0, 10.0])):
     data = load_higgs_train()
-    nn = NeuralNetwork(data)
-    data = []
-    for i in range(500):
-        nn.train(train_epoch=5)
-        total_epochs, trn, tst = nn.estimate_error()
-        data.append([total_epochs, trn, tst])
-    err_df = pd.DataFrame.from_records(data, columns=['iteration', 'training_error', 'test_error'])
-    return err_df
+
+    def estimate_error(nn):
+        error_data = []
+        for i in range(n_iterations):
+            nn.train(train_epoch=1)
+            total_epochs, trn, tst = nn.estimate_error()
+            error_data.append([nn.learning_rate, total_epochs, trn, tst])
+        return error_data
+
+    records = [estimate_error(NeuralNetwork(data, l)) for l in learning_rate_range]
+    err_df = pd.DataFrame.from_records(records, columns=['learning_rate', 'iteration', 'training_error', 'test_error'])
+    err_df['training_accuracy'] = 1 - err_df['training_error'] / 100
+    err_df['test_accuracy'] = 1 - err_df['test_error'] / 100
+    return err_df.set_index('iteration')
 
 
-def plot_accuracy_function(df, title):
-    smooth_df = pd.rolling_mean(df, 5)
+def plot_accuracy_function(df, title='Accuracy f (iterations)'):
+    smooth_df = pd.rolling_mean(df[['training_accuracy', 'test_accuracy']], 5)
     smooth_df.plot(title=title)
 

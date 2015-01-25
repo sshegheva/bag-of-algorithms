a285c0eb178df2ca32971b16f7380371f0dd4638
Snejana Shegheva <sshegheva@dataxu.com>
2015-01-25 13:59:03 -0500

    create archive script

--- bin/build_assignment_artifact.sh
+++ bin/build_assignment_artifact.sh
@@ -3,5 +3,10 @@
 # scripts which creates the archive for submitting the assignment
 
 cd /tmp
-git clone https://github.com/sshegheva/bag-of-algorithms
+rm -rf /tmp/sshegheva3
+git clone https://github.com/sshegheva/bag-of-algorithms /tmp/sshegheva3
 
+cp /tmp/sshegheva3/analysis/README.md /tmp/sshegheva3/README.md
+cp "/tmp/sshegheva3/analysis/notebooks/Supervised Classification.pdf" "/tmp/sshegheva3/sshegheva3-analysis.pdf"
+
+tar -cvf sshegheva3.tar.gz sshegheva3

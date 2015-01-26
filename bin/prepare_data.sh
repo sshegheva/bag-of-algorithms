8c4d1f7b07db13d91c30965c92d438fe9642a7b2
Snejana Shegheva <sshegheva@dataxu.com>
2015-01-25 19:50:25 -0500

    Fix plumbing

--- bin/prepare_data.sh
+++ bin/prepare_data.sh
@@ -3,11 +3,7 @@
 # define vavriables
 PROJECT_ROOT=..
 
-TRAINING_DATA=${PROJECT_ROOT}/data/training
-TEST_DATA=${PROJECT_ROOT}/data/test
+TRAINING_DATA=${PROJECT_ROOT}/data
 
 unzip  ${TRAINING_DATA}/higgs_training.zip -d ${TRAINING_DATA}
 unzip  ${TRAINING_DATA}/bidding_training.csv.zip -d ${TRAINING_DATA}
-
-unzip ${TEST_DATA}/higgs_test.zip -d ${TEST_DATA}
-unzip ${TEST_DATA}/bidding_test.csv.zip -d ${TEST_DATA}

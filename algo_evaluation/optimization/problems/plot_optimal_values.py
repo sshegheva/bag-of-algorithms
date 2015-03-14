8d727c8829b9d1201659f05292536ffdb0c9876b
Snejana Shegheva <sshegheva@dataxu.com>
2015-03-14 14:33:33 -0400

    Plot clock times for the algorithms

--- algo_evaluation/optimization/problems/plot_optimal_values.py
+++ algo_evaluation/optimization/problems/plot_optimal_values.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def plot_optimal_values(rhc_df, sa_df, ga_df, mm_df):
@@ -12,3 +13,17 @@ def plot_optimal_values(rhc_df, sa_df, ga_df, mm_df):
     mm_df.groupby('iteration').max()['optimal_value'].plot(title='MIMIC', ax=ax[1][1], legend=False, sharex=False)
     ax[1][1].set_ylabel("optimal value")
     plt.tight_layout()
+
+
+def collect_and_plot_clocktimes(experiment_results):
+    algos = ['RHC', 'SA', 'GA', 'MIMIC']
+    domains = ['WaldoOpt', 'HyperOpt', 'CronOpt']
+    data = []
+    for dom in experiment_results:
+        times = [algo['time'].mean() for algo in dom]
+        data.append(times)
+    df = pd.DataFrame.from_records(data, columns=algos)
+    df.rename(index={i: domains[i] for i in range(len(df))}, inplace=True)
+    df.plot(kind='barh', figsize=(8, 4))
+    plt.xlabel('Time (sec)')
+    return df

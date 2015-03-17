1a69cb3aa1f8acd4f261b32ef47b0a302b3ae9d3
Snejana Shegheva <sshegheva@dataxu.com>
2015-03-17 18:49:51 -0400

    Plot feature importances

--- algo_evaluation/feature_selection/feature_importance_eval.py
+++ algo_evaluation/feature_selection/feature_importance_eval.py
@@ -1,15 +1,19 @@
 """
 http://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html#example-ensemble-plot-forest-importances-py
 """
+import pandas as pd
 from sklearn.ensemble import ExtraTreesClassifier
 
 
-def test(data, n_estimators=250):
+def rank_features(data, n_estimators=250):
     features, weights, labels = data
+    feature_names = features.columns.tolist()
     # Build a forest and compute the feature importances
     forest = ExtraTreesClassifier(n_estimators=n_estimators,
                                   random_state=0)
 
     forest.fit(features, labels)
     importances = forest.feature_importances_
-    return importances
+    importances = zip(feature_names, importances)
+    df = pd.DataFrame(importances, columns=['feature', 'rank']).set_index('feature').sort('rank', ascending=False)
+    return df

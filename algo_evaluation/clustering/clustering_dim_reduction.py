b27f39d5af80b48a58262c494f82c980298ed73a
Snejana Shegheva <sshegheva@dataxu.com>
2015-03-29 15:04:58 -0400

    wrap up clustering analysis

--- algo_evaluation/clustering/clustering_dim_reduction.py
+++ algo_evaluation/clustering/clustering_dim_reduction.py
@@ -2,10 +2,12 @@
 Reproduce your clustering experiments,
 but on the data after you've run dimensionality reduction on it.
 """
+from time import time
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn import mixture
-from algo_evaluation.feature_selection import pca_eval
+from algo_evaluation.feature_selection import pca_eval, rand_projections, ica_eval, trunk_svd
 from algo_evaluation.feature_selection import reduce_dimensions
 from algo_evaluation.clustering import kmeans_eval
 from algo_evaluation.clustering import plot_cluster_estimation
@@ -60,8 +62,10 @@ def evaluate_conv_clustering(bid_data, n_components):
 
 
 def kmeans_transform(higgs_data, n_clusters, n_components, display=False):
+    start = time()
     reduced_higgs_data, elapsed = pca_eval.transform(higgs_data, n_components=n_components)
     cluster_data = KMeans(n_clusters=n_clusters).fit_transform(reduced_higgs_data)
+    elapsed = time() - start
     data = {'features': cluster_data, 'weights': higgs_data[1], 'labels': higgs_data[2]}
     if display and n_clusters == 2:
         df = pd.DataFrame.from_records(cluster_data, columns=['new_feature_' + str(n) for n in range(n_clusters)])
@@ -70,7 +74,40 @@ def kmeans_transform(higgs_data, n_clusters, n_components, display=False):
                                       kind='scatter', color='darkgreen', label='signal')
         df[df.label == 'b'].plot(x='new_feature_0', y='new_feature_1',
                                  kind='scatter', color='darkred', ax=ax, label='background')
-    return data
+    return data, elapsed
+
+
+def compare_cluster_runtime(data, n_clusters, n_components):
+    t0 = time()
+    features = data[0]
+    KMeans(n_clusters=n_clusters).fit_transform(features)
+    t1 = time() - t0
+    t0 = time()
+    mixture.GMM(n_components=n_clusters).fit(features)
+    t2 = time() - t0
+    reduced_higgs_data, t3 = pca_eval.transform(data, n_components=n_components)
+    reduced_higgs_data, t4 = rand_projections.transform(data, n_components=n_components)
+    reduced_higgs_data, t5 = ica_eval.transform(data, n_components=n_components)
+    reduced_higgs_data, t6 = trunk_svd.transform(data, n_components=n_components)
+    t0 = time()
+    KMeans(n_clusters=n_clusters).fit_transform(reduced_higgs_data)
+    t7 = time() - t0
+    t0 = time()
+    mixture.GMM(n_components=n_clusters).fit(reduced_higgs_data)
+    t8 = time() - t0
+    ser = pd.Series([t1, t2, t3, t4, t5, t6, t7, t8], index=['original Kmeans clustering',
+                                                     'original GMM clustering',
+                                                     'PCA', 'RCA', 'ICA', 'LSA',
+                                                     'reduced Kmeans clustering',
+                                                     'reduced GMM clustering'])
+    ser.name = 'time'
+    return ser
+
+
+def plot_running_time(higgs_time, conv_time):
+    f, ax = plt.subplots(1, 2, figsize=(12, 3))
+    higgs_time.plot(kind='bar', ax=ax[0], title='Higgs Runtime Comparison')
+    conv_time.plot(kind='bar', ax=ax[1], title='Converters Runtime Comparison')
 
 
 def neural_net_post_clustering(data):

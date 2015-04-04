18dd1f16e70198251dcbc514d294aa1d32616e26
Snejana Shegheva <sshegheva@dataxu.com>
2015-04-04 12:54:30 -0400

    Fix plumbing issues

--- bin/build_assignment_artifact.sh
+++ bin/build_assignment_artifact.sh
@@ -7,6 +7,6 @@ rm -rf /tmp/sshegheva3
 git clone https://github.com/sshegheva/bag-of-algorithms /tmp/sshegheva3
 
 cp /tmp/sshegheva3/analysis/TASK3_README.md /tmp/sshegheva3/README.md
-cp "/tmp/sshegheva3/analysis/notebooks/Unsupervised Learning and Dimensionality Reduction.pdf.pdf" "/tmp/sshegheva3/sshegheva3-analysis.pdf"
+cp "/tmp/sshegheva3/analysis/notebooks/Unsupervised Learning and Dimensionality Reduction.pdf" "/tmp/sshegheva3/sshegheva3-analysis.pdf"
 
 tar -cvf sshegheva3.tar.gz sshegheva3
